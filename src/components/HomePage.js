60f9ec8010d4c5f3a42774ddc55db2bd7629cc00
Angela <angela@Angelas-MacBook-Pro.local>
2018-09-21 22:48:21 -0400

    cleared homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,18 +3,13 @@ import {
   Link
 } from 'react-router-dom';
 
-const HomePage = () => {
-  return (
-    <div>
-      <h1>React Slingshot</h1>
 
-      <h2>Get Started</h2>
-      <ol>
-        <li>Review the <Link to="/fuel-savings">demo app</Link></li>
-        <li>Remove the demo and start coding: npm run remove-demo</li>
-      </ol>
-    </div>
-  );
+const Parent = styled.div `
+  display: flex;
+`
+
+const HomePage = () => {
 };
+
 
 export default HomePage;
